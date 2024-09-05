d13c783360554d8b7feb277e88b375dbe340b0da
Line Katralen <105557930+Line-Kat@users.noreply.github.com>
2024-09-05 20:17:11 +0200

    small change to getParentById in controller, returning status 404 insted of null and status 200 when parent is not found

--- user/src/main/java/com/chores/user/controller/ParentController.java
+++ user/src/main/java/com/chores/user/controller/ParentController.java
@@ -2,6 +2,8 @@ package com.chores.user.controller;
 
 import com.chores.user.model.Parent;
 import com.chores.user.service.ParentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,8 +16,10 @@ public class ParentController {
     }
 
     @GetMapping("/parent/{id}")
-    public Parent getParentById(@PathVariable Long id) {
-        return parentService.getParentById(id);
+    public ResponseEntity<Parent> getParentById(@PathVariable Long id) {
+        return parentService.getParentById(id)
+                .map(parent -> new ResponseEntity<>(parent, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/parent")
