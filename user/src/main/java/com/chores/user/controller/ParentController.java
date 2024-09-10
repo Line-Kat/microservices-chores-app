f118b66f6dacc209e4236694160bb696d03dfd34
Line Katralen <105557930+Line-Kat@users.noreply.github.com>
2024-09-10 11:04:47 +0200

    to the controller classes, I added the annotations requestMapping and requiredArgsContructor

--- user/src/main/java/com/chores/user/controller/ParentController.java
+++ user/src/main/java/com/chores/user/controller/ParentController.java
@@ -2,27 +2,26 @@ package com.chores.user.controller;
 
 import com.chores.user.model.Parent;
 import com.chores.user.service.ParentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/parent")
+@RequiredArgsConstructor
 public class ParentController {
 
     private final ParentService parentService;
 
-    public ParentController(ParentService parentService) {
-        this.parentService = parentService;
-    }
-
-    @GetMapping("/parent/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Parent> getParentById(@PathVariable Long id) {
         return parentService.getParentById(id)
                 .map(parent -> new ResponseEntity<>(parent, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping("/parent")
+    @PostMapping
     public Parent createParent(@RequestBody Parent parent) {
         return parentService.createParent(parent);
     }
