22f352d84542f1b1169b83fe66c6471b9c5a725b
Line Katralen <105557930+Line-Kat@users.noreply.github.com>
2024-09-10 13:03:00 +0200

    added the annotation Slf4j

--- user/src/main/java/com/chores/user/controller/ChildController.java
+++ user/src/main/java/com/chores/user/controller/ChildController.java
@@ -3,6 +3,7 @@ package com.chores.user.controller;
 import com.chores.user.model.Child;
 import com.chores.user.service.ChildService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/child")
 @RequiredArgsConstructor
+@Slf4j
 public class ChildController {
 
     private final ChildService childService;
--- user/src/main/java/com/chores/user/controller/ParentController.java
+++ user/src/main/java/com/chores/user/controller/ParentController.java
@@ -3,6 +3,7 @@ package com.chores.user.controller;
 import com.chores.user.model.Parent;
 import com.chores.user.service.ParentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/parent")
 @RequiredArgsConstructor
+@Slf4j
 public class ParentController {
 
     private final ParentService parentService;
