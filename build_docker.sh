5047acaaf2f2031421965d61c774fc3efd31e74a
Line Katralen <105557930+Line-Kat@users.noreply.github.com>
2024-12-10 10:17:18 +0100

    debugging

--- build_docker.sh
+++ build_docker.sh
@@ -4,9 +4,7 @@ USER_IMAGE_NAME="user:0.0.1"
 REWARD_IMAGE_NAME="reward:0.0.1"
 CHILDCHORE_IMAGE_NAME="childchore:0.0.1"
 CHORES_IMAGE_NAME="chores:0.0.1"
-
-docker build -t "${GATEWAY_IMAGE_NAME}" ./gateway
-docker build -t "${CONSUL_IMPORTER_IMAGE_NAME}" ./docker/consul
+docker build -t "${GATEWAY_IMAGE_NAME}" ./gateway docker build -t "${CONSUL_IMPORTER_IMAGE_NAME}" ./docker/consul
 docker build -t "${USER_IMAGE_NAME}" ./user
 docker build -t "${REWARD_IMAGE_NAME}" ./reward
 docker build -t "${CHILDCHORE_IMAGE_NAME}" ./childchore
