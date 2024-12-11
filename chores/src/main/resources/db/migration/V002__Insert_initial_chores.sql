c1473240dcd5fdbd9ee1ddcd0f7bd83779c74c79
Line Katralen <105557930+Line-Kat@users.noreply.github.com>
2024-12-11 10:43:02 +0100

    refactored the service Gateway

--- chores/src/main/resources/db/migration/V002__Insert_initial_chores.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO chore (chore_id, chore_uuid, chore_name)
-VALUES
-    (1, 'a325dad1-723b-457c-94e2-1c3b730a06a8', 'Vacuum'),
-    (2, 'e7fbb6dc-7e14-4e0f-9ba3-bc9f741e8727', 'Take out the trash'),
-    (3, '7d6f9b22-925c-4b85-81b2-a1b9b86d4f3b', 'Clean your room')
